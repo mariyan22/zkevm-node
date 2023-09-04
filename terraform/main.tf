0d33da209f1f8352da801f0690dea81f95e95920
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-04 19:14:00 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,14 @@ provider "aws" {
 
 resource "aws_instance" "evm_nodes" {
   count         = 3
-  ami           = "ami-0766f68f0b06ab145" 
+  ami           = "ami-0766f68f0b06ab145"
   instance_type = "t2.micro"
   tags = {
     Name = "evm-node-${count.index}"
   }
+}
+
+# Create an output variable to store the list of IP addresses
+output "ec2_instance_ips" {
+  value = [for instance in aws_instance.evm_nodes : instance.private_ip]
 }

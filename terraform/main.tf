bc483af877d7b58077be7c1c64c87482c7a21ca7
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-04 11:46:05 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# main.tf
+
+provider "aws" {
+  region = "eu-central-1"  # Choose your desired region
+}
+
+resource "aws_instance" "evm_nodes" {
+  count         = 3
+  ami           = "ami-0766f68f0b06ab145" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "evm-node-${count.index}"
+  }
+}

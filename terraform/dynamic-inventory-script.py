744c312185d784a252d7cbefc16421007329d5ad
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-05 02:24:48 +0300

    Update dynamic-inventory-script.py

--- terraform/dynamic-inventory-script.py
+++ terraform/dynamic-inventory-script.py
@@ -10,25 +10,16 @@ output = subprocess.check_output(terraform_cmd).decode('utf-8')
 # Parse Terraform output
 terraform_output = json.loads(output)
 
-# Create a list of EC2 instance IDs
-instance_ids = terraform_output['ec2_instance_ids']['value']
-
-# Initialize a dictionary to store the public IP addresses
-public_ip_addresses = {}
-
-# Use the AWS CLI to fetch the public IP addresses of the EC2 instances
-for instance_id in instance_ids:
-    aws_cli_cmd = ['aws', 'ec2', 'describe-instances', '--instance-ids', instance_id, '--query', 'Reservations[0].Instances[0].PublicIpAddress', '--output', 'text']
-    public_ip = subprocess.check_output(aws_cli_cmd).decode('utf-8').strip()
-    public_ip_addresses[instance_id] = public_ip
+# Create a list of IP addresses
+ip_addresses = terraform_output['ec2_instance_ips']['value']
 
 # Generate the dynamic inventory in JSON format
 inventory = {
     'evm_nodes': {
         'hosts': {
             f"evm-node-{count}": {
-                'ansible_host': public_ip_addresses[instance_id]
-            } for count, instance_id in enumerate(instance_ids)
+                'ansible_host': ip_addresses[count]
+            } for count in range(len(ip_addresses))
         },
         'vars': {}
     },

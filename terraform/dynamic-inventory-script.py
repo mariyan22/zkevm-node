15deed82623ff6f477ee9fd1de25a122735266c7
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-05 01:34:16 +0300

    Update dynamic-inventory-script.py

--- terraform/dynamic-inventory-script.py
+++ terraform/dynamic-inventory-script.py
@@ -10,11 +10,16 @@ output = subprocess.check_output(terraform_cmd).decode('utf-8')
 # Parse Terraform output
 terraform_output = json.loads(output)
 
+# Create a list of IP addresses
+ip_addresses = terraform_output['ec2_instance_ips']['value']
+
 # Generate the dynamic inventory in JSON format
 inventory = {
     'evm_nodes': {
         'hosts': {
-            f"evm-node-{count}": {} for count, _ in enumerate(terraform_output['ec2_instance_ips']['value'])
+            f"evm-node-{count}": {
+                'ansible_host': ip_addresses[count]
+            } for count in range(len(ip_addresses))
         },
         'vars': {}
     },

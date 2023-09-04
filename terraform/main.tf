c5454e0e086ad2ffb99034066d7f8d8a42288f5e
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-05 01:48:39 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,11 @@ resource "aws_instance" "evm_nodes" {
   instance_type = "t2.micro"
   key_name      = "ec2-key-pair"
 
+  # Associate the security group with the EC2 instances
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+
   tags = {
-    Name = "evm-node-${count.index}"
+    Name     = "evm-node-${count.index}"
     Hostname = "evm-node-${count.index}"
   }
 }

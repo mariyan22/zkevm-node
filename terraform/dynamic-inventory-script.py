cfa5fe71d9e6f52d487523dcd9578b8836958495
mariyan22 <131192925+mariyan22@users.noreply.github.com>
2023-09-04 20:35:04 +0300

    Update dynamic-inventory-script.py

--- terraform/dynamic-inventory-script.py
+++ terraform/dynamic-inventory-script.py
@@ -3,22 +3,29 @@
 import subprocess
 import json
 
-# Run Terraform to get the EC2 instances' output
-terraform_cmd = ['terraform', 'output', '-json']
-output = subprocess.check_output(terraform_cmd).decode('utf-8')
+try:
+    # Run Terraform to get the EC2 instances' output
+    terraform_cmd = ['terraform', 'output', '-json']
+    output = subprocess.check_output(terraform_cmd).decode('utf-8')
 
-# Parse Terraform output
-terraform_output = json.loads(output)
+    # Parse Terraform output
+    terraform_output = json.loads(output)
 
-# Generate the dynamic inventory in JSON format
-inventory = {
-    'evm_nodes': {
-        'hosts': terraform_output['ec2_instance_ips']['value'],
-        'vars': {}
-    },
-    '_meta': {
-        'hostvars': {}
-    }
-}
+    # Check if the 'ec2_instance_ips' output variable exists
+    if 'ec2_instance_ips' in terraform_output:
+        # Generate the dynamic inventory in JSON format
+        inventory = {
+            'evm_nodes': {
+                'hosts': terraform_output['ec2_instance_ips']['value'],
+                'vars': {}
+            },
+            '_meta': {
+                'hostvars': {}
+            }
+        }
 
-print(json.dumps(inventory))
+        print(json.dumps(inventory))
+    else:
+        print("Error: 'ec2_instance_ips' not found in Terraform output.")
+except Exception as e:
+    print(f"Error: {e}")
